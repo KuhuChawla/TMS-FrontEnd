abc61af9a071bf9cff3ecd17dfb8683da3427048
Kuhu Chawla <72125043+KuhuChawla@users.noreply.github.com>
2021-09-15 00:20:01 +0530

    profile

--- Front-end/team_management_software/lib/views/screens/account.dart
+++ Front-end/team_management_software/lib/views/screens/account.dart
@@ -1,15 +1,164 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
 
 class Account extends StatefulWidget {
   const Account({Key? key}) : super(key: key);
 
   @override
-  _DashboardState createState() => _DashboardState();
+  _AccountState createState() => _AccountState();
 }
 
-class _DashboardState extends State<Account> {
+class _AccountState extends State<Account> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: const Text("ACCOUNT"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              child: Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Icon(
+                      Icons.account_circle,
+                      size: 100,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 8,
+                    height: 10,
+                  ),
+                  Expanded(
+                    child: Text(
+                      "Username",
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Organisation",
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(
+                        Icons.work,
+                        size: 38,
+                      ),
+                      title: Text('My Company'),
+                      subtitle: Text('KSVR'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Notifications",
+                ),
+              ),
+            ),
+            Container(
+              child: Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(
+                        Icons.do_disturb_on_outlined,
+                        size: 38,
+                      ),
+                      title: Text('Do not disturb'),
+                      subtitle: Text('Off'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(
+                        Icons.crop_square,
+                        size: 38,
+                      ),
+                      title: Text('Push'),
+                      subtitle: Text('Manage'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Support",
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(
+                        Icons.info_outline,
+                        size: 38,
+                      ),
+                      title: Text('Android Guide'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Card(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    const ListTile(
+                      leading: Icon(
+                        Icons.help_outline,
+                        size: 38,
+                      ),
+                      title: Text('Contact Support'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
