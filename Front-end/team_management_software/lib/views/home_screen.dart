b3bf85044c1afb6637a7b1c7ab96540cb9a83c5a
Kuhu Chawla <72125043+KuhuChawla@users.noreply.github.com>
2021-09-14 22:06:45 +0530

    ok

--- Front-end/team_management_software/lib/views/home_screen.dart
+++ Front-end/team_management_software/lib/views/home_screen.dart
@@ -1,13 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:team_management_software/views/chat_section/chatting_screen.dart';
+
 import 'package:team_management_software/views/chat_section/coversation_list.dart';
 import 'package:team_management_software/views/project_screen.dart';
-import 'package:team_management_software/views/screens/account.dart';
-import 'package:team_management_software/views/screens/dashboard.dart';
-import 'package:team_management_software/views/screens/inbox.dart';
-import 'package:team_management_software/views/screens/mytasks.dart';
-import 'package:team_management_software/views/screens/search.dart';
-import 'package:team_management_software/views/test_screen.dart';
+
 import 'chat_section/push_notification.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -59,55 +54,3 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class MyStatefulWidget extends StatefulWidget {
-  const MyStatefulWidget({Key? key}) : super(key: key);
-
-  @override
-  State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
-}
-
-/// This is the private State class that goes with MyStatefulWidget.
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  int _selectedIndex = 0;
-  final screen = [HomeScreen(), MyTasks(), Inbox(), Search(), Account()];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-              backgroundColor: Colors.black),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.splitscreen),
-              label: 'MyTasks',
-              backgroundColor: Colors.black),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.add_alert),
-              label: 'Inbox',
-              backgroundColor: Colors.black),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Search',
-              backgroundColor: Colors.black),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.account_circle),
-              label: 'Account',
-              backgroundColor: Colors.black),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.amber[800],
-        onTap: _onItemTapped,
-      ),
-      body: screen[_selectedIndex],
-    );
-  }
-}
